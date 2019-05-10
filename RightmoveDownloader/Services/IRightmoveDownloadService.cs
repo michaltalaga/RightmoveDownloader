439852bd08f2b069f126af494e8549f9f4fd4231
Michał Talaga <michal.talaga@mountainwarehouse.com>
2019-05-10 17:43:58 +0100

    Add project files.

---
+++ RightmoveDownloader/Services/IRightmoveDownloadService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RightmoveDownloader.Services
+{
+	public interface IRightmoveDownloadService
+	{
+		void Download(string postcode, decimal radius, int minBedrooms, int maxBedrooms, int minPrice, int maxPrice);
+	}
+}
---
+++ RightmoveDownloader/Startup.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.Common;
+using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RightmoveDownloader.Services;
+
+namespace RightmoveDownloader
+{
+    public class Startup
+    {
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services)
+        {
+			services.AddHttpClient();
+			services.AddTransient<IRightmoveDownloadService, RightmoveDownloadService>();
+			services.AddHangfire(config =>
+			{
+				config.UseMemoryStorage();
+			});
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IRecurringJobManager recurringJobManager)
+        {
+			app.UseHangfireDashboard();
+			app.UseHangfireServer();
+			recurringJobManager.AddOrUpdate("Download", Job.FromExpression<IRightmoveDownloadService>(service => service.Download("CR0 6EF", 20, 2, 3, 1400, 1800)), Cron.Yearly(2, 31));
+
+		}
+	}
+}
