a93280ac64169e367e838eb6e1afefc9c1948f26
Michał Talaga <michal.talaga@mountainwarehouse.com>
2019-05-13 13:38:56 +0100

    Added GoogleSheetsPropertyRepository skeleton

---
+++ RightmoveDownloader/Services/GoogleSheetsPropertyRespository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RightmoveDownloader.Services
+{
+	public class GoogleSheetsPropertyRespository : IPropertyRepository
+	{
+		public void AddProperty(RightmoveHttpClient.Property property)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
