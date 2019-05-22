f05c69086fcc73ec8a469ee910cb6cba0af0d725
Michał Talaga <michal.talaga@mountainwarehouse.com>
2019-05-22 15:00:52 +0100

    i still break things

--- RightmoveDownloader/Startup.cs
+++ RightmoveDownloader/Startup.cs
@@ -52,7 +52,7 @@ namespace RightmoveDownloader
 			services.AddTransient<IPropertyRepository, GoogleSheetsPropertyRespository>();
 			services.AddHangfire(config =>
 			{
-				i break things
+				i still break things 
 				if (Directory.Exists("Hangfire"))
 				{
 					config.UseLiteDbStorage(Path.Combine("Hangfire", "hangfire.db"));
