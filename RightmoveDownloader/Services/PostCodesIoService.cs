77e7e38c356c82f0833cc6a18d5b6f44be402183
Michal Talaga <mikeon@objectware.pl>
2021-09-22 00:21:28 +0100

    empty implementation of PostCodesIoService

---
+++ RightmoveDownloader/Services/PostCodesIoService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Logging;
+using RightmoveDownloader.Repositories;
+using System.Threading.Tasks;
+
+namespace RightmoveDownloader.Services
+{
+    public class PostCodesIoService : IPostCodeService
+    {
+        private readonly IPropertyRepository propertyRepository;
+        private readonly ILogger<PostCodesIoService> logger;
+
+        public PostCodesIoService(IPropertyRepository propertyRepository, ILogger<PostCodesIoService> logger)
+        {
+            this.propertyRepository = propertyRepository;
+            this.logger = logger;
+        }
+        public async Task FindPostCodes()
+        {
+            logger.LogInformation($"FindPostCodes()");
+            var locations = await propertyRepository.GetLocations(false);
+            logger.LogInformation($"FindPostCodes() - DONE");
+        }
+    }
+}
